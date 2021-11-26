bfff270601d9c89d89502716e0740f69761acf85
Bartek <75174234+Areczekk9@users.noreply.github.com>
2021-11-26 02:52:34 -0800

    Update testip.js

--- testip.js
+++ testip.js
@@ -1,7 +1,33 @@
 var http = require('http');
 
+var net = require("net");
+
+var server = net.createServer();
+
 http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
   resp.on('data', function(ip) {
     console.log("My public IP address is: " + ip);
   });
 });
+
+server.on("connection", function (socket) {
+    var remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
+    console.log("New client connection is made %s", remoteAddress);
+
+    socket.on("data", function (d) {
+        console.log("Data from %s: %s", remoteAddress, d);
+        socket.write("Hello " + d);
+    });
+
+    socket.once("close", function () {
+        console.log("Connection from %s closed", remoteAddress);
+    });
+
+    socket.on("error", function (err) {
+        console.log("Connection %s error: %s", remoteAddress, err.message);
+    });
+});
+
+server.listen(80, function() {
+    console.log("Server listening to %j", server.address());
+})
